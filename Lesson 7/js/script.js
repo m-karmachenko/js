c66368bbcec64a85563282d1ad443e35ffa3f218
Mikhail Karmachenko <carmachenko@yandex.ru>
2022-06-07 13:52:12 +0300

    Initial commit JS

---
+++ Lesson 7/js/script.js
@@ -0,0 +1,45 @@
+// let number = 5;
+// const leftBorderWidth = 1;
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 10;
+// console.log(obj);
+
+// const obj = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj.name);
+// console.log(obj['name']);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+// console.log(arr[1]);
+
+// const arr = ['a', 'b', 'c'];
+// const arrObj = {
+//     0: 'a',
+//     1: 'b',
+//     2: 'c'
+// };
+
+// console.log(arr[1]);
+// console.log(arrObj.a);
+
+// const obj = {
+//     Anna: 500,
+//     'Alice': 800
+// };
+
+// console.log(obj.Anna);
+
+// alert('helloworld');
+
+const result = confirm('are you here');
+console.log(result);
